0dc3b409b741c0cb563820d8cce49c3d0a99550e
SaloniPardeshi1412 <saloni.p@binyuga.com>
2023-12-05 10:25:46 +0530

    added dropdown to navbar.

--- lib/screens/landing_page_screens/bottom_nav_bar/bottom_nav_bar.dart
+++ lib/screens/landing_page_screens/bottom_nav_bar/bottom_nav_bar.dart
@@ -1,150 +1,162 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:country_picker/country_picker.dart';
-
-class BottomNavBarScreen extends StatelessWidget {
+import 'package:dropdown_button2/dropdown_button2.dart';
+class BottomNavBarScreen extends StatefulWidget {
   const BottomNavBarScreen({super.key});
 
   @override
+  State<BottomNavBarScreen> createState() => _BottomNavBarScreenState();
+}
+
+class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
+  String selectedCountry = 'Select Country/regions';
+  List<String> countries = [
+    'Select Country/regions',
+    'United States',
+    'Canada',
+    'United Kingdom',
+    'Germany',
+    'France',
+    'India',
+    'Japan',
+    // Add more countries as needed
+  ];
+  String? selectedValue;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 187,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(
-                'images/black_bg.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 5),
-              child: Image.asset(
-                'images/binyuga_logo.png',
-              ),
+        body:Container(
+          height: 187,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'images/black_bg.png'),
+              fit: BoxFit.cover,
             ),
+          ),
+          child: Row(
+              children: [
+          Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Image.asset(
+            'images/binyuga_logo.png',
+          ),
+              ),
 
-            //  SizedBox(width: 70,),
-            const Padding(
-              padding: EdgeInsets.only(top: 65),
-              child: Column(
+                SizedBox(width: 70,),
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        FontAwesomeIcons.facebook,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Icon(
-                        FontAwesomeIcons.youtube,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Icon(
-                        FontAwesomeIcons.twitter,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Icon(
-                        FontAwesomeIcons.instagram,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Icon(
-                        FontAwesomeIcons.linkedin,
-                        color: Colors.white,
-                        size: 20,
-                      ),
-                    ],
+                  Icon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.white,
+                    size: 20,
                   ),
                   SizedBox(
-                    height: 45,
+                    width: 20,
                   ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 80),
-                        child: Text(
-                            ' All rights reserved by Binyuga. Copyright ©2023',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                            )),
-                      ),
-                    ],
+                  Icon(
+                    FontAwesomeIcons.youtube,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    FontAwesomeIcons.twitter,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    FontAwesomeIcons.instagram,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    FontAwesomeIcons.linkedin,
+                    color: Colors.white,
+                    size: 20,
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              width: 60,
-            ),
-            const Row(
-              children: [
-                Text('www.career@binyuga.com',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Inter',
-                      fontSize: 18,
-                    )),
-              ],
-            ),
-            const SizedBox(
-              width: 250,
-            ),
-
-            // Padding(
-            //   padding: const EdgeInsets.only(top: 65),
-            //   child: Column(
-            //     children: [
-            //       ElevatedButton(
-            //         style: ButtonStyle(
-            //           backgroundColor: MaterialStateProperty.all(Colors.white),
-            //         ),
-            //         child: const Text(
-            //           'Select Country/region               ▼',
-            //           style: TextStyle(
-            //               fontSize: 11,
-            //               color: Colors.black,
-            //               fontFamily: 'Inter'),
-            //         ),
-            //         onPressed: () {
-            //           showCountryPicker(
-            //             context: context,
-            //             showPhoneCode: true,
-            //             showSearch: true,
-            //             onSelect: (Country country) {
-            //               ScaffoldMessenger.of(context).showSnackBar(
-            //                 SnackBar(
-            //                   content: Text(country.name),
-            //                 ),
-            //               );
-            //             },
-            //           );
-            //         },
-            //       ),
-            //     ],
-            //   ),
-            // ),
+              const SizedBox(
+          width: 90,
+              ),
+              const Row(
+          children: [
+            Text('www.career@binyuga.com',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Inter',
+                  fontSize: 18,
+                )),
           ],
-        ),
-      ),
+              ),
+              const SizedBox(
+          width: 350,
+              ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                      border:
+                      Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(2)),
+                  //padding: EdgeInsets.all(3),
+                  width: MediaQuery.of(context).size.width / 6,
+                  height: 30,
+               child: DropdownButton(
+                 focusColor: Colors.white,
+                  alignment: Alignment.center,
+                  hint: Text(countries[0],
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  ),),
+                  value: selectedCountry,
+                 underline: const SizedBox(),
+                 isExpanded: true,
+                 icon: const Icon(
+                   Icons.arrow_drop_down,
+                   size: 20,
+                 ),
+                 onChanged: (String? newValue) {
+                   setState(() {
+                     selectedCountry = newValue!;
+                   });
+                 },
+                  items: countries.map((country) {
+                    return DropdownMenuItem(
+                      value: country,
+                      child: Center(child: Text(country,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black
+                      ),)),
+                    );
+                  }).toList(),
+
+                ),),
+              ],
+          ),
+            ),
     );
   }
 }
+
 
