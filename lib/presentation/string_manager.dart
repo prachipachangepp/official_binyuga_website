d29760cc7e1911ef143076151f20354d20d20f8e
SaloniPardeshi1412 <saloni.p@binyuga.com>
2023-12-05 13:08:51 +0530

    constant files added.

---
+++ lib/presentation/string_manager.dart
@@ -0,0 +1,16 @@
+class AppString {
+  /// Home screen
+  static const String signinhead = "Sign In";
+
+  /// About Us screen
+
+  /// description screen
+
+  /// Approach screen
+
+  /// Team Member screen
+
+  /// Union screen
+
+  ///Bottom Navbar screen
+}
