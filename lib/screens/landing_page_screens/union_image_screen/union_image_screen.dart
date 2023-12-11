a54a999392ba21d15ae2619932709cc634287a8c
DELL <patipuja186@gmail.com>
2023-12-11 17:43:56 +0530

    updated position

--- lib/screens/landing_page_screens/union_image_screen/union_image_screen.dart
+++ lib/screens/landing_page_screens/union_image_screen/union_image_screen.dart
@@ -11,7 +11,7 @@ class UnionImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 2200,
+      height: 2200, width: MediaQuery.of(context).size.width,
       color: ColorManager.white,
       child: Stack(
         children: [
@@ -23,16 +23,18 @@ class UnionImageScreen extends StatelessWidget {
             width: MediaQuery.of(context).size.width,
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 100),
+            padding:  EdgeInsets.only(left:  MediaQuery.of(context).size.width/20,top:15,),
             child: Row(
               children: [
                 Image.network(
                   "images/union_1.png",
-                  height: 720,
+                  height: 700,
                   // width: MediaQuery.of(context).size.width * 0.8,
                   width: MediaQuery.of(context).size.width / 2,
                 ),
-                SizedBox(width: MediaQuery.of(context).size.width/50,),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 50,
+                ),
                 Text(
                   AppString.unionTxt1,
                   style: UnionTxtScreen1.union1TextStyle(context),
@@ -42,12 +44,12 @@ class UnionImageScreen extends StatelessWidget {
           ),
 
           Padding(
-            padding: const EdgeInsets.only(left: 50,bottom: 20),
+            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width /30,right: MediaQuery.of(context).size.width /40,bottom:280,),
             child: Row(
               children: [
                 Image.network(
                   "images/rectangle.png",
-                  height: 780,
+                  height: 730,
                   // width: MediaQuery.of(context).size.width * 0.8,
                   width: MediaQuery.of(context).size.width / 1.9,
                 ),
@@ -58,18 +60,21 @@ class UnionImageScreen extends StatelessWidget {
           Center(
             child: Text(
               AppString.unionTxt2,
+              //textAlign: TextAlign.center,
               style: UnionTxtScreen2.union2TextStyle(context),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 1200.0,left: 120),
+            padding: const EdgeInsets.only(top: 1200.0, left: 10),
             child: Row(
               children: [
                 Text(
                   AppString.unionTxt3,
                   style: UnionTxtScreen1.union1TextStyle(context),
                 ),
-                SizedBox(width: MediaQuery.of(context).size.width/50,),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 50,
+                ),
                 Image.network(
                   "images/union_2.png",
                   height: 720,
@@ -80,9 +85,8 @@ class UnionImageScreen extends StatelessWidget {
             ),
           ),
 
-
           Padding(
-            padding: const EdgeInsets.only(top: 1150,left: 600),
+            padding: EdgeInsets.only(top: 1150,right: MediaQuery.of(context).size.width / 50,left: MediaQuery.of(context).size.width /2.2,),
             child: Image.network(
               "images/rectangle.png",
               height: 820,
@@ -90,7 +94,6 @@ class UnionImageScreen extends StatelessWidget {
               width: MediaQuery.of(context).size.width / 1.9,
             ),
           ),
-
         ],
       ),
     );
